58112148241b5846a1011c04ac9c17e173bed834
Khedidja <khadidja.bouchouareb@gmail.com>
2025-06-02 15:56:06 +0000

    depevhetoi

---
+++ Strings/main.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+
+int main() {
+   char * first_name = "John";
+   char last_name[] = "Doe";
+   char name[100];
+
+   last_name[0] = 'B';
+   printf(name, "%s %s", first_name, last_name);
+   if (strncmp(name, "John Boe", 100) == 0) {
+       printf("Done!\n");
+   }
+   name[0]='\0';
+   strncat(name,first_name,4);
+   strncat(name,last_name,20);
+   printf("%s\n",name);
+   return 0;
+}
